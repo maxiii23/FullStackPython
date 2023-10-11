2abf57084c7d999cf2dc987a57e9d5f5ccd5e60c
maxiii23 <maximiliano.cardenas.hernandez@alumnos.sip.cl>
2023-10-11 13:02:16 -0300

    commit

--- flask_mysql/crud/Usuarios-CRUD-modularizado_copy/flask_app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-# Crear una instancia de la aplicación Flask
-app = Flask(__name__)
-
-# Configurar una clave secreta para la aplicación (necesaria para ciertas funcionalidades)
-app.secret_key = 'Mi llave secreta'
